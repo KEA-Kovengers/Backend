4b03320fcbed3c24b3e79a5f2864bda42b16f850
wooing1084 <kmjung1515@naver.com>
2024-04-07 16:22:31 +0900

    feat: test commit for jenkins pipeline

--- user-service/src/main/java/com/newcord/userservice/UserserviceApplication.java
+++ user-service/src/main/java/com/newcord/userservice/UserserviceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class UserserviceApplication {
-	//Test for Jenkins
 	public static void main(String[] args) {
 		SpringApplication.run(UserserviceApplication.class, args);
 	}
