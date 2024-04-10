0f1f7a923a31fdd10aa741e43cebbed01a7aa597
wooing1084 <kmjung1515@naver.com>
2024-04-11 00:28:31 +0900

    feat: Mongo Base Entity Getter 추가

--- article-service/src/main/java/com/newcord/articleservice/global/common/BaseMongoTimeEntity.java
+++ article-service/src/main/java/com/newcord/articleservice/global/common/BaseMongoTimeEntity.java
@@ -1,10 +1,12 @@
 package com.newcord.articleservice.global.common;
 
 import java.time.LocalDateTime;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Getter
 @Document
 public abstract class BaseMongoTimeEntity {
     @CreatedDate
