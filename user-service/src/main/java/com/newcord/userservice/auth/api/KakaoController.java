c1cc7e40d3baf1864424de7101da59a430e0744c
남소미 <somi4219@gmail.com>
2024-05-14 06:08:23 +0900

    feat : 폴더 기능 생성

--- user-service/src/main/java/com/newcord/userservice/auth/api/KakaoController.java
+++ user-service/src/main/java/com/newcord/userservice/auth/api/KakaoController.java
@@ -1,7 +1,7 @@
 package com.newcord.userservice.auth.api;
 
 import com.newcord.userservice.auth.jwt.JwtTokenProvider;
-import com.newcord.userservice.auth.response.ApiResponse;
+import com.newcord.userservice.global.common.response.ApiResponse;
 import com.newcord.userservice.auth.service.KakaoAuthService;
 import com.newcord.userservice.auth.utils.KakaoTokenJsonData;
 import com.newcord.userservice.auth.utils.dto.KakaoTokenResponse;
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 
 @Controller
-@RequestMapping("/api/auth")
+@RequestMapping("/users/auth")
 @RequiredArgsConstructor
 @Slf4j
 public class KakaoController {
@@ -34,7 +34,7 @@ public class KakaoController {
     public ApiResponse<HashMap<String, String>> kakaoOauth(@RequestParam("code") String code) {
         log.info("인가 코드를 이용하여 토큰을 받습니다.");
         KakaoTokenResponse kakaoTokenResponse = kakaoTokenJsonData.getToken(code);
-        log.info("토큰에 대한 정보입니다.{}",kakaoTokenResponse);
+        log.info("토큰에 대한 정보입니다.{}", kakaoTokenResponse);
 
         // 카카오 토큰을 사용하여 회원가입 여부를 확인하고 Jwt 토큰을 발급
         ApiResponse<HashMap<String, String>> response = kakaoAuthService.authCheck(kakaoTokenResponse.getAccess_token());
@@ -50,16 +50,15 @@ public class KakaoController {
         log.info("해독된 유저 아이디: {}", userIdString);
 
         // 유저 아이디를 Long 타입으로 변환
-        Long userId = null;
-        userId = Long.parseLong(userIdString);
+        Long userId = Long.parseLong(userIdString);
 
         // 유저 아이디를 가지고 디비에 있는지 확인
         boolean userExists = kakaoAuthService.isUserExists(userId);
         if (!userExists) {
             log.error("User not found in database with ID: {}", userId);
-            return ApiResponse.error("User not found in database");
+            return ApiResponse.onFailure("USER_NOT_FOUND", "User not found in database", null);
         }
-        return ApiResponse.success(userId, "User ID retrieved successfully.");
+        return ApiResponse.onSuccess(userId);
     }
 
     // jwt 엑세스 토큰 유효성을 검증
@@ -68,7 +67,7 @@ public class KakaoController {
     public ApiResponse<Boolean> validateToken(@RequestHeader("X-AUTH-TOKEN") String jwtToken) {
         boolean isValid = jwtTokenProvider.validateToken(jwtToken);
         log.info("JWT 토큰 유효성 검증 결과: {}", isValid);
-        return ApiResponse.success(isValid, "Token validation successful.");
+        return ApiResponse.onSuccess(isValid);
     }
 
     // jwt 리프레시 토큰 유효할 때 jwt 엑세스 토큰 재발급
@@ -81,10 +80,10 @@ public class KakaoController {
             HashMap<String, String> map = new HashMap<>();
             map.put("accessToken", accessToken);
             log.info("발급된 액세스 토큰: {}", accessToken);
-            return ApiResponse.success(map, "Access token issued successfully.");
+            return ApiResponse.onSuccess(map);
         } else {
             log.error("JWT 리프레시 토큰이 유효하지 않습니다.");
-            return ApiResponse.error("Refresh token validation failed.");
+            return ApiResponse.onFailure("REFRESH_TOKEN_INVALID", "Refresh token validation failed.", null);
         }
     }
 
--- user-service/src/main/java/com/newcord/userservice/folder/repository/FolderPostRepository.java
+++ user-service/src/main/java/com/newcord/userservice/folder/repository/FolderPostRepository.java
@@ -1,10 +1,20 @@
 package com.newcord.userservice.folder.repository;
 
+import com.newcord.userservice.folder.domain.Folder;
 import com.newcord.userservice.folder.domain.FolderPost;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface FolderPostRepository extends JpaRepository<FolderPost, Long> {
 
+    @Query("SELECT fp FROM FolderPost fp WHERE fp.folder_id = :folder_id")
+    List<FolderPost> findByFolder_id(Long folder_id);
+
+    @Query("SELECT fp FROM FolderPost fp WHERE fp.folder_id = :folder_id AND fp.post_id = :post_id")
+    FolderPost findByFolder_idAndPost_id(Long folder_id, Long post_id);
 }
