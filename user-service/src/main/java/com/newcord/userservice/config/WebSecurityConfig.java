f25fb9e92b9e034b19e84a2777f38838046ec383
남소미 <somi4219@gmail.com>
2024-05-23 21:06:51 +0900

    fix : cors 에러 수정

--- user-service/src/main/java/com/newcord/userservice/config/WebSecurityConfig.java
+++ user-service/src/main/java/com/newcord/userservice/config/WebSecurityConfig.java
@@ -17,12 +17,13 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 public class WebSecurityConfig {
     private final JwtTokenProvider jwtTokenProvider;
-    private final CorsConfig corsConfig;
+//    private final CorsConfig corsConfig;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable) //csrf 비활성화
+                .cors(AbstractHttpConfigurer::disable)
                 // 세션 사용 안함
                 .sessionManagement((sessionManagement) ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -32,7 +33,7 @@ public class WebSecurityConfig {
                         requests.requestMatchers("/users/auth/**","/users/auth/login","/","/index.html","/v3/api-docs/**","/swagger-ui/index.html","/swagger-ui/**", "/swagger-resources/**").permitAll()
                         // 나머지 모든 API는 Jwt 인증 필요
                         .anyRequest().authenticated())
-                .addFilter(corsConfig.corsFilter()) //CorsFilter 등록
+//                .addFilter(corsConfig.corsFilter()) //CorsFilter 등록
                 // Http 요청에 대한 Jwt 유효성 선 검사
                 .addFilterBefore(new JwtAuthFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
 
