0374636ca7ad53e8baa9b8fee6d172cf180dc3e1
남소미 <somi4219@gmail.com>
2024-05-23 21:27:57 +0900

    fix : cors 에러 수정

--- user-service/src/main/java/com/newcord/userservice/config/CorsConfig.java
+++ user-service/src/main/java/com/newcord/userservice/config/CorsConfig.java
@@ -1,27 +1,27 @@
-//package com.newcord.userservice.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-//import org.springframework.web.filter.CorsFilter;
-//
-//import java.util.List;
-//
-//@Configuration
-//public class CorsConfig {
-//    @Bean
-//    public CorsFilter corsFilter() {
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        CorsConfiguration config = new CorsConfiguration();
-//        config.setAllowCredentials(true);
-//        config.addAllowedOrigin("*");
-//        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
-//        config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
-//        config.addExposedHeader("Custom-Header");
-//        config.setMaxAge(3600L);
-//        source.registerCorsConfiguration("/**",config);
-//
-//        return new CorsFilter(source);
-//    }
-//}
+package com.newcord.userservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:3000");
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        config.addExposedHeader("Custom-Header");
+        config.setMaxAge(3600L);
+        source.registerCorsConfiguration("/**",config);
+
+        return new CorsFilter(source);
+    }
+}
