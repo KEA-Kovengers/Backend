30c50a35292dcc8db3ada360b5b98ff2b7b2d74e
wooing1084 <kmjung1515@naver.com>
2024-05-24 15:25:28 +0900

    Update webconfig for cors

--- article-service/src/main/java/com/newcord/articleservice/config/WebConfig.java
+++ article-service/src/main/java/com/newcord/articleservice/config/WebConfig.java
@@ -3,9 +3,13 @@ package com.newcord.articleservice.config;
 import com.newcord.articleservice.global.resolver.UserArgumentResolver;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -17,10 +21,10 @@ public class WebConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000")
-            .allowedMethods("GET", "POST", "PUT", "DELETE")
-            .allowedHeaders("Authorization", "Content-Type")
-            .exposedHeaders("Custom-Header")
+            .allowedOrigins("*")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .exposedHeaders("*")
             .maxAge(3600);
     }
     @Override
