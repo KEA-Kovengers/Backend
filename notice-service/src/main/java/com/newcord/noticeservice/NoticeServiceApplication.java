b0f770389e6402b54c3504d38827ef935d4570fa
남소미 <somi4219@gmail.com>
2024-05-24 20:16:58 +0900

    feat : add dockerfile, yml

--- notice-service/src/main/java/com/newcord/noticeservice/NoticeServiceApplication.java
+++ notice-service/src/main/java/com/newcord/noticeservice/NoticeServiceApplication.java
@@ -2,8 +2,13 @@ package com.newcord.noticeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableMongoAuditing
+@EnableMongoRepositories(basePackages = "com.newcord.noticeservice")
 public class NoticeServiceApplication {
 
 	public static void main(String[] args) {
