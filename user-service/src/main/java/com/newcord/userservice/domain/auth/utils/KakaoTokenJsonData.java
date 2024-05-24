412f7a9d7281983a046eb127b4957181b30fed71
남소미 <somi4219@gmail.com>
2024-05-24 12:46:26 +0900

    fix : 카카오 로그인 수정

--- user-service/src/main/java/com/newcord/userservice/domain/auth/utils/KakaoTokenJsonData.java
+++ user-service/src/main/java/com/newcord/userservice/domain/auth/utils/KakaoTokenJsonData.java
@@ -14,7 +14,7 @@ public class KakaoTokenJsonData {
     private final WebClient webClient;
 
 //    @Value("${kakao.redirect_uri}")
-    private String REDIRECT_URI="http://localhost:8081/users/auth/login";
+    private String REDIRECT_URI="http://http://172.16.211.43/users/auth/login";
 
     @Value("${kakao.api_key}")
     private String CLIENT_ID;
