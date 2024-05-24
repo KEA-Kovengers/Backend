ebc7afed4ab36112ea95f60748b4ede82f49b011
2sojeong <dlgur200123@naver.com>
2024-05-25 02:49:03 +0900

    fix: slave 디비 문제

--- user-service/src/main/java/com/newcord/userservice/domain/user/service/UsersCommandServiceImpl.java
+++ user-service/src/main/java/com/newcord/userservice/domain/user/service/UsersCommandServiceImpl.java
@@ -9,9 +9,11 @@ import com.newcord.userservice.domain.user.dto.UsersRequest.UsersRequestDTO;
 import com.newcord.userservice.domain.user.dto.UsersResponse.UsersResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class UsersCommandServiceImpl implements UsersCommandService{
     public final UsersRepository usersRepository;
 
