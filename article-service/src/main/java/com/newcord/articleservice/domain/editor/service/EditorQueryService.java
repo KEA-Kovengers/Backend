928cd528813e92c3d537c06d637178c9ece165e9
2sojeong <dlgur200123@naver.com>
2024-05-31 20:44:49 +0900

    feat:좋아요 수, 댓글 수 조회

--- article-service/src/main/java/com/newcord/articleservice/domain/editor/service/EditorQueryService.java
+++ article-service/src/main/java/com/newcord/articleservice/domain/editor/service/EditorQueryService.java
@@ -7,7 +7,7 @@ import com.newcord.articleservice.domain.posts.dto.PostResponse.PostListResponse
 import java.util.List;
 
 public interface EditorQueryService {
-    PostListResponseDTO getPostListByUserID(Long userID, Integer page, Integer size);     //편집자 게시글 목록 조회
+    EditorPostListResponseDTO getPostListByUserID(Long userID, Integer page, Integer size);     //편집자 게시글 목록 조회
     Editor getEditorByPostIdAndUserID(Long postId, Long userID);     //편집자 조회
     EditorListResponseDTO getAllEditorsByPostId(Long postId);     //게시글의 모든 편집자 조회
 
--- article-service/src/main/java/com/newcord/articleservice/domain/editor/service/EditorQueryServiceImpl.java
+++ article-service/src/main/java/com/newcord/articleservice/domain/editor/service/EditorQueryServiceImpl.java
@@ -1,54 +1,56 @@
 package com.newcord.articleservice.domain.editor.service;
 
+import com.newcord.articleservice.domain.comments.repository.CommentsRepository;
 import com.newcord.articleservice.domain.editor.dto.EditorResponse.*;
 import com.newcord.articleservice.domain.editor.dto.EditorResponse.EditorListResponseDTO;
 import com.newcord.articleservice.domain.editor.entity.Editor;
 import com.newcord.articleservice.domain.editor.repository.EditorRepository;
-import com.newcord.articleservice.domain.posts.dto.PostResponse.PostListResponseDTO;
+import com.newcord.articleservice.domain.likes.repository.LikeRepository;
 import com.newcord.articleservice.domain.posts.enums.PostStatus;
 import com.newcord.articleservice.global.common.exception.ApiException;
 import com.newcord.articleservice.global.common.response.code.status.ErrorStatus;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class EditorQueryServiceImpl implements EditorQueryService{
     private final EditorRepository editorRepository;
+    private final LikeRepository likeRepository;
+    private final CommentsRepository commentsRepository;
 
     @Override
-    public PostListResponseDTO getPostListByUserID(Long userID, Integer page, Integer size) {
+    public EditorPostListResponseDTO getPostListByUserID(Long userID, Integer page, Integer size) {
         PageRequest pageRequest = PageRequest.of(page, size);
-
+        List<EditorPostResponseDTO> result=new ArrayList<>();
         Page<Editor> editors = editorRepository.findByUserID(userID,PostStatus.POST, pageRequest);
 
-
-        return PostListResponseDTO.builder()
-                .postList(editors)
+        for(Editor editor:editors){
+           int likeCnt=likeRepository.findAllByPost_id(editor.getPost().getId()).size();
+           int commentCnt=commentsRepository.findByPostId(editor.getPost().getId()).size();
+            EditorPostResponseDTO editorPostResponseDTO=EditorPostResponseDTO.builder()
+                    .postList(editor)
+                    .likeCnt(likeCnt)
+                    .commentCnt(commentCnt)
+                    .build();
+            result.add(editorPostResponseDTO);
+        }
+        return EditorPostListResponseDTO.builder()
+                .editorPostResponseDTOS(result)
                 .build();
-//        Page<Editor> editors = editorRepository.findByUserID(userID,PostStatus.POST, pageRequest);
-//        List<Editor> filteredEditors = editors.getContent().stream()
-//                .collect(Collectors.toList());
-//
-//        Page<Editor> filteredEditorsPage = new PageImpl<>(filteredEditors, pageRequest, filteredEditors.size());
-//
-//        return PostListResponseDTO.builder()
-//                .postList(filteredEditorsPage)
-//                .build();
-    }
 
+
+    }
     @Override
     public Editor getEditorByPostIdAndUserID(Long postId, Long userID) {
         Editor editor = editorRepository.findByPostIdAndUserID(postId, userID).orElse(null);
