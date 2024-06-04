7f73dfc397f6f7921019fbda1c04567aa6998ac0
남소미 <somi4219@gmail.com>
2024-06-04 13:50:18 +0900

    feat : 알림 유저서비스/게시글서비스 적용

--- article-service/src/main/java/com/newcord/articleservice/domain/comments/repository/CommentsRepository.java
+++ article-service/src/main/java/com/newcord/articleservice/domain/comments/repository/CommentsRepository.java
@@ -4,8 +4,10 @@ import com.newcord.articleservice.domain.comments.entity.Comments;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface CommentsRepository extends JpaRepository<Comments,Long> {
@@ -16,4 +18,6 @@ public interface CommentsRepository extends JpaRepository<Comments,Long> {
     @Query("SELECT c FROM Comments c WHERE c.user_id =:userID")
     List<Comments> findByUser_id(Long userID);
     //List<Comments> findByPostIdOrderByComment_idAsc(Long postId);
-}
+
+    @Query("SELECT c FROM Comments c WHERE c.comment_id = :commentId")
+    Optional<Comments> findByCommentId(@Param("commentId") Long commentId);}
