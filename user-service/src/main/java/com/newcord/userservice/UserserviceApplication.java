05e503fb49aadc4bfe79e53fe9cfb81e7d7a8d73
wooing1084 <kmjung1515@naver.com>
2024-06-08 15:55:40 +0900

    feat: 유저 조회 캐싱

--- user-service/src/main/java/com/newcord/userservice/UserserviceApplication.java
+++ user-service/src/main/java/com/newcord/userservice/UserserviceApplication.java
@@ -3,10 +3,12 @@ package com.newcord.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication(exclude={SecurityAutoConfiguration.class})
+@EnableCaching
 public class UserserviceApplication {
 	//test comment
 	public static void main(String[] args) {
